0afe7a24f323e527b91b1211d19b53b0e98b3634
Joseph Fahey <fahey.joseph@gmail.com>
2015-02-02 16:54:49 +0100

    failing tests to get started

--- test/gmark/core_test.clj
+++ test/gmark/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [gmark.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def simple-fragment
+  {:tag :lg
+   :attrs {:type "stanza"}
+   :content [{:tag :l
+              :attrs {:n 1}
+              :content ["stuff I like to say"]}
+             {:tag :l
+              :attrs {:n 2}
+              :content ["I say it every day"]}]})
+
+
+(deftest test-transform-lg
+  (let [mrk (to-gmark simple-fragment)]
+    (is (= mrk "[n=1] stuff I like to say\n[n=2]I say it every day"))))
