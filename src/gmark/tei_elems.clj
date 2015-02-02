af4493410c64e597d01fc40b54e2d78a55b6e09e
Joseph Fahey <fahey.joseph@gmail.com>
2015-02-02 16:55:23 +0100

    tei-elems, working for simple markup output

---
+++ src/gmark/tei_elems.clj
@@ -0,0 +1,78 @@
+(ns gmark.tei-elems)
+
+(defrecord ContainerEType [contains])
+(defrecord ChunkEType [contains line-start])
+                                        
+;;; no contains: what it can contain is determined by parent type
+(defrecord InnerEType [begin end])      
+(defrecord EmptyEType [sym])
+
+(defprotocol EditableType
+  (text-edit-as-child? [etype])
+  (can-contain-text? [etype]))
+
+(extend-type ContainerEType
+  EditableType
+  (text-edit-as-child? [_] false)
+  (can-contain-text? [_] false))
+
+(extend-type InnerEType
+  EditableType
+  (text-edit-as-child? [_] true)
+  (can-contain-text? [_] true))
+
+(extend-type ChunkEType
+  EditableType
+  (text-edit-as-child? [_] true)
+  (can-contain-text? [_] true))
+
+(extend-type EmptyEType
+  EditableType
+  (text-edit-as-child? [_] true)
+  (can-contain-text? [_] false))
+
+(defprotocol TextType
+  (begin-text [etype])
+  (end-text [etype]))
+
+(extend-type InnerEType
+  TextType
+  (begin-text [etype] (:begin etype))
+  (end-text [etype] (:end etype)))
+
+(extend-type ChunkEType
+  TextType
+  (begin-text [etype] (str "\n" (:line-start etype)))
+  (end-text [etype] "\n"))
+
+(extend-type EmptyEType
+  TextType
+  (begin-text [etype] (:sym etype))
+  (end-text [_] nil))
+
+(defn container-type [contains] (ContainerEType. contains))
+(defn chunk-type [contains line-start] (ChunkEType. contains line-start))
+(defn inner-type [begin end] (InnerEType. begin end))
+(defn empty-type [sym]  (EmptyEType. sym))
+
+(defn elem-to-text [elem tagtypes]
+  (let [etype ((:tag elem) tagtypes)
+        content (:content elem)]
+    (str (begin-text etype)
+     (apply str (map
+                  #(if (string? %)
+                     %
+                     (elem-to-text % tagtypes))
+                  content))
+      (end-text etype))))
+
+(def tei
+  {:body    (container-type [:div])
+   :text    (container-type [:div])
+   :caesura (empty-type "|")
+   :rhyme   (inner-type "//" "//")
+   :div     (container-type [:div :head :lg])
+   :head    (chunk-type [] "*")
+   :lg      (container-type [:l])
+   :l       (chunk-type [:rhyme :caesura] "-")
+   :note    (inner-type "{" "}")})
---
+++ test/gmark/tei_elems_test.clj
@@ -0,0 +1,40 @@
+(ns gmark.tei-elems-test
+  (:require [clojure.test :refer :all]
+            [gmark.tei-elems :refer :all]))
+
+
+(deftest test-basic-protocol-setup
+  "clear our throat and check our protocols"
+  (let [chunk (chunk-type [:lg] "-")]
+    (is (= true (text-edit-as-child? chunk)))
+    (is (= true (can-contain-text? chunk)))))
+
+
+(deftest test-chunk-to-text
+  "output gmarkup from l (verse line) element"
+  (let [base-elem {:tag :l
+                   :attrs {}
+                   :content []}
+        all-text-line (elem-to-text
+                        (assoc base-elem :content ["My verse"])
+                        tei)
+        nested-cesure (elem-to-text
+                        (assoc base-elem
+                          :content ["My "
+                                    {:tag :caesura :attrs {} :content []}
+                                    "cesure"])
+                        tei)
+        nested-note (elem-to-text
+                      (assoc base-elem
+                        :content ["My "
+                                  {:tag :note
+                                   :attrs {}
+                                   :content ["annotated"]}
+                                  " verse"])
+                      tei)]
+    (is (= all-text-line "\n-My verse\n"))
+    (is (= nested-cesure "\n-My |cesure\n"))
+    (is (= nested-note "\n-My {annotated} verse\n"))))
+
+
+
