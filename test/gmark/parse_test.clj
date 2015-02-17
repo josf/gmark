9fa1d3013cdc911fd09af64926db9a5c7f394e45
Joseph Fahey <fahey.joseph@gmail.com>
2015-02-17 14:40:43 +0100

    fix test ns and finalize simple tokenize test

--- test/gmark/parse_test.clj
+++ test/gmark/parse_test.clj
@@ -1,8 +1,16 @@
-(ns gmark.parse_test
+(ns gmark.parse-test
   (:require [clojure.test :refer :all]
             [gmark.parse :refer :all]))
 
 (deftest tokenize-simple
   (is (=
-        (tokenize "the //stuff// we like")
-        ["the " {:token "//" :type :either} "stuff" {:token "//" :type :either} " we like"])))
+        (tokenize "the //stuff// we like"  [["//" "//"]])
+        ["the " {:token "//" :type :either} "stuff" {:token "//" :type :either} " we like"])
+    (=
+      (tokenize "the //stuff// we {{like}}" [["//" "//"] ["{{" "}}"]])
+      ["the " {:token "//" :type :either} "stuff"
+       {:token "//" :type :either} " we " {:token "{{" :type :begin} "like"
+       {:token "}}" :type :end}])))
+
+
+
