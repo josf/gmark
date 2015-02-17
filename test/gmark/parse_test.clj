df80a07fe01da44d51d37390e9acf30e2d1aabb1
Joseph Fahey <fahey.joseph@gmail.com>
2015-02-17 14:33:14 +0100

    remove old tests

--- test/gmark/parse_test.clj
+++ test/gmark/parse_test.clj
@@ -2,14 +2,6 @@
   (:require [clojure.test :refer :all]
             [gmark.parse :refer :all]))
 
-(deftest test-combined-regex
-  (is (= "(//)|(\\*)" (combined-regex-pattern ["//" "*"])))
-  (is (= "(//)|(\\*)" (combined-regex-pattern ["//" "*" "*" "//"]))))
-
-(deftest test-combined-regex-escaping
-  (is (= "(//)|(\\*)|(\\{)|(\\})")
-    (combined-regex-pattern ["//" "*" "{" "}"])))
-
 (deftest tokenize-simple
   (is (=
         (tokenize "the //stuff// we like")
