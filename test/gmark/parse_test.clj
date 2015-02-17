ee7af1034450c5ef0da34bb0bdf65bd1de9b2dc9
Joseph Fahey <fahey.joseph@gmail.com>
2015-02-17 15:22:48 +0100

    test for structure
    Fairly basic...

--- test/gmark/parse_test.clj
+++ test/gmark/parse_test.clj
@@ -19,4 +19,22 @@
 (deftest match-by-length-test-neg
   (is (nil? (match-by-length "blah//" ["//" "//"]))))
 
-
+(deftest structure-simple
+  (let [token-map  {"//"
+                    {:tag :em :closing-tag "//"}
+                    "|"
+                    {:tag :caesura :no-content true}}
+        tokenized ["stuff" {:token "//" :type :either} "we"
+                   {:token "//" :type :either} "like "
+                   {:token "|" :type :either} " do"]
+        structured (structure
+                     tokenized
+                     {:tag :root :attrs {} :content []}
+                     token-map)]
+    (is (map? structured))
+    (is (= (:tag structured) :root))
+    (is (= "stuff" (first (:content structured))))
+    (is (= {:tag :em :attrs {} :content ["we"]} (second (:content structured))))
+    (is (= {:tag :caesura :attrs {} :content []}
+          (nth (:content structured) 3)))
+    (is (= " do" (last (:content structured))))))
